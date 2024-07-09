a8f78df94092760f3fee4ab3b83ffe40f1645ff4
danpatoneil <danpatoneil@gmail.com>
2024-07-08 20:20:16 -0400

    changed message to textarea

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -77,24 +77,19 @@ export default function Contact() {
           </div>
         </div>
         <div className="flex flex-wrap -mx-3 mb-6">
-          <div className="w-full px-3">
-            <label
-              className="block uppercase tracking-wide text-black_olive-500 text-xs font-bold mb-2"
-              htmlFor="grid-message"
-            >
-              Message
-            </label>
-            <input
-            name='message'
-              className="appearance-none block w-full bg-spanish_orange-900 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-message"
-              type="text"
-              placeholder="What can I help you with?"
-              onChange={handleInputChange}
-            />
-          </div>
-
-        </div>
+  <div className="w-full px-3">
+    <label className="block uppercase tracking-wide text-black_olive-500 text-xs font-bold mb-2" htmlFor="grid-message">
+      Message
+    </label>
+    <textarea
+      name='message'
+      className="resize-y appearance-none block w-full bg-spanish_orange-900 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+      id="grid-message"
+      placeholder="What can I help you with?"
+      onChange={handleInputChange}
+    ></textarea>
+  </div>
+</div>
         <button type="submit" className="bg-emerald-400 text-isabelline-500 border-2 border-black_olive-500">Submit</button>
       </form>
           {errorMessage && (
